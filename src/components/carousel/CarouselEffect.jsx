b31b0e141c783d7fa9814ab41d1da6ff7cebaa4e
gemechuH <gemechuhunduma100@gmail.com>
2025-05-22 04:22:20 -0800

    some add

--- src/components/carousel/CarouselEffect.jsx
+++ src/components/carousel/CarouselEffect.jsx
@@ -8,29 +8,29 @@ import { Link } from "react-router-dom";
 const CarouselEffect = () => {
   const slides = [
     {
-      image: imgs[0],
+      // image: imgs[0],
       greating: "WELCOME TO",
       description:
         "Digitalize your business with internationally validated solutions designed not only for efficiency and sustainability of operational processes, but also for scalability, growth, and alignment with your business priorities. Stop adapting to software, let the software adapt to you.",
       aboutUs: "About Us",
       lang: "English",
     },
-    {
-      image: imgs[1],
-      greating: "እንኳን በደህና መጡ",
-      description:
-        "በአለም አቀፍ ደረጃ በተረጋገጡ መፍትሄዎች የንግድዎን ዲጂታላይዝ ያድርጉ፣ የተነደፉት ለስራ ሂደቶች ብቻ ሳይሆን ለእድገት፣ ለማስፋፋት እና ከንግድዎ ቅድሚያዎች ጋር ለማጣጣም ነው። ለሶፍትዌር መላመድ ያቁሙ፣ ሶፍትዌሩ ለእርስዎ ይላመድ።",
-      aboutUs: "ስለ እኛ",
-      lang: "Amharic",
-    },
-    {
-      image: imgs[2],
-      greating: "BAGA NAGAAN DHUFTAN",
-      description:
-        "Daldalaa keessan furtuuwwan idil-addunyaatti mirkanaawaniin diijitaalaayizii godhaa, kan dizaayinii isaanii kan hojii adeemsa qofaaf osoo hin taane, guddina, bal'ina fi sagantaa daldala keessanii waliin wal simsiisuu dha. Softweeriif of madaqsuun dhaabaa, softweerin isin haa madaqu.",
-      aboutUs: "Waa'ee Keenya",
-      lang: "Afaan Oromo",
-    },
+    // {
+    //   // image: imgs[1],
+    //   greating: "እንኳን በደህና መጡ",
+    //   description:
+    //     "በአለም አቀፍ ደረጃ በተረጋገጡ መፍትሄዎች የንግድዎን ዲጂታላይዝ ያድርጉ፣ የተነደፉት ለስራ ሂደቶች ብቻ ሳይሆን ለእድገት፣ ለማስፋፋት እና ከንግድዎ ቅድሚያዎች ጋር ለማጣጣም ነው። ለሶፍትዌር መላመድ ያቁሙ፣ ሶፍትዌሩ ለእርስዎ ይላመድ።",
+    //   aboutUs: "ስለ እኛ",
+    //   lang: "Amharic",
+    // },
+    // {
+    //   // image: imgs[2],
+    //   greating: "BAGA NAGAAN DHUFTAN",
+    //   description:
+    //     "Daldalaa keessan furtuuwwan idil-addunyaatti mirkanaawaniin diijitaalaayizii godhaa, kan dizaayinii isaanii kan hojii adeemsa qofaaf osoo hin taane, guddina, bal'ina fi sagantaa daldala keessanii waliin wal simsiisuu dha. Softweeriif of madaqsuun dhaabaa, softweerin isin haa madaqu.",
+    //   aboutUs: "Waa'ee Keenya",
+    //   lang: "Afaan Oromo",
+    // },
   ];
 
   return (
@@ -72,11 +72,11 @@ const CarouselEffect = () => {
       >
         {slides.map((slide, index) => (
           <div key={index} className={classes.slide}>
-            <img
+            {/* <img
               src={slide.image}
               alt={`slide-${index}`}
               className={classes.image}
-            />
+            /> */}
             <div className={classes.overlay}>
               <div className={classes.content}>
                 <h3 className={classes.greeting}>{slide.greating}</h3>
